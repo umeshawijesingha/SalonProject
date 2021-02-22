1b15090bbadfc55c0c978c221580bb4336cf47e6
umeshawijesingha <umeshawijesingha@gmail.com>
2021-02-22 18:59:40 +0530

    Footer details added

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -1,22 +1,52 @@
 import React from 'react'
 import classes from './Footer.module.css'
+import {FaFacebook} from 'react-icons/fa';
+import {FaTwitter} from 'react-icons/fa';
+import {FaLinkedin} from 'react-icons/fa';
 
 function Footer(){
 
     return(
-        <div className={classes.Footer}>
-            <div className={classes.FooterContent}>
-
-            </div>
-         <hr></hr>
-            
+        <div className={classes.footer}>
+           
+           <ul>
             <ul>
-                <li>About Us</li>
+                <li>Home</li>
+                <li>About</li>
                 <li>Services</li>
-                <li></li>
+                <li>Gallery</li>
+                <li>Contact Us</li>
             </ul>
 
+            
+            <ul>
+            
+                <li>Contact Us</li>
+                <li>0111224558</li>
+                <li>info@nishi.com</li>
+                <li>www.salonnishi.com</li>
+                <li> </li>
+            </ul>
 
+            <ul>
+                <li>No.86,</li>
+                <li>Station rd,</li>
+                <li>Bambalapitiya,</li>
+                <li>Colombo 4</li>
+               
+            </ul>
+            
+            
+            </ul>
+
+            <h2 className={classes.follow}>Follow Us</h2>
+
+        <div className={classes.icons}>
+            <h1><FaFacebook/></h1>
+            <h1><FaTwitter/></h1>
+            <h1><FaLinkedin/></h1>
+
+        </div>
         </div>
     );
 }
